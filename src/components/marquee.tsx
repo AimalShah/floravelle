aeca34b3fdb550128f4526106090f23306a2fe56
aimalshah <aimalshah62@gmail.com>
2026-03-09 00:55:15 +0500

    initial commit

---
+++ src/components/marquee.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react"
+import gsap from "gsap";
+import "../styles/global.css"
+
+
+export default function Marquee() {
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(".marquee-track", {
+        xPercent: -50,
+        duration: 15,
+        ease: "none",
+        repeat: -1
+      });
+    }, marqueeRef);
+
+    return () => ctx.revert();
+  }, [])
+  return (
+    <div ref={marqueeRef} className="py-8 border-y bg-[#E8E4DE] overflow-hidden text-nowrap">
+      <div
+        className="marquee-track flex whitespace-nowrap w-max"
+      >
+        <span className="text-7xl font-primary font-thin text-nowrap mx-8">DAILY ELEGANCE ✦ SIGNATURE SCENTS ✦  PURE AROMAS ✦</span>
+        <span className="text-7xl font-primary font-thin text-nowrap">DAILY ELEGANCE ✦ SIGNATURE SCENTS ✦  PURE AROMAS ✦</span>
+      </div>
+    </div>
+  )
+}
